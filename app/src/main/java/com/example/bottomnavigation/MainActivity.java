a23b8413b2c33a79744cb7fc078602d86473a3d6
JnSsanRico <juan0096150@gmail.com>
2024-06-10 22:02:09 -0500

    codificacion en el build.gradle se agrego el android.enableJetifier=true properties

--- app/src/main/java/com/example/bottomnavigation/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.bottomnavigation;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+
+import com.example.bottomnavigation.Fragment.DashFragment;
+import com.example.bottomnavigation.Fragment.HomeFragment;
+import com.example.bottomnavigation.Fragment.MapsFragment;
+import com.example.bottomnavigation.Fragment.ProfileFragment;
 import com.example.bottomnavigation.databinding.ActivityMainBinding;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,22 +21,41 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.bottomNavigationView); // Assuming your BottomNavigationView has this ID
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setBackground(null);
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.home, R.id.map, R.id.cart, R.id.settings, R.id.profile) // Use your actual menu item IDs
-                .build();
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment); // Assuming you have a NavHostFragment with this ID
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
 
-        // Remove the old fragment handling logic (replaceFragment, setOnItemSelectedListener, etc.)
+                case R.id.pefil:
+                    replaceFragment(new ProfileFragment());
+                    break;
+
+                case R.id.map:
+                    replaceFragment(new MapsFragment());
+                    break;
+
+                case R.id.settings:
+                    replaceFragment(new DashFragment());
+                    break;
+            }
+
+            return true;
+
+        });
+
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
