76fd982628c4bfcc9ea752da5835f2dfc6bd6931
JnSsanRico <juan0096150@gmail.com>
2024-06-16 16:05:34 -0500

    funciona el mapa

--- app/src/main/java/com/example/bottomnavigation/Fragment/MapsFragment.java
+++ app/src/main/java/com/example/bottomnavigation/Fragment/MapsFragment.java
@@ -1,70 +1,138 @@
 package com.example.bottomnavigation.Fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.bottomnavigation.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-public class MapsFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
-    private EditText txtLatitud, txtLongitud;
-    private GoogleMap mMap;
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
+
+    FrameLayout map;
+    GoogleMap gMap;
+    Location currentLocation;
+    Marker marker;
+    FusedLocationProviderClient fusedClient;
+    private static final int REQUEST_CODE = 101;
+    SearchView searchView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_maps, container, false);
 
-        txtLatitud = view.findViewById(R.id.txtLatitud);
-        txtLongitud = view.findViewById(R.id.txtLongitud);
+        map = container.findViewById(R.id.map);
+        searchView = view.findViewById(R.id.search);
+//        searchView.clearFocus();
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(this);
-        }
+        fusedClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+        getLocation();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String loc = searchView.getQuery().toString();
+                if (loc == null) {
+                    Toast.makeText(requireContext(), "Location Not Found", Toast.LENGTH_SHORT).show();
+                } else {
+                    Geocoder geocoder = new Geocoder(requireContext(), Locale.getDefault());
+                    try {
+                        List<Address> addressList = geocoder.getFromLocationName(loc, 1);
+                        if (addressList.size() > 0) {
+                            LatLng latLng = new LatLng(addressList.get(0).getLatitude(), addressList.get(0).getLongitude());
+                            if (marker != null) {
+                                marker.remove();
+                            }
+                            MarkerOptions markerOptions = new MarkerOptions().position(latLng).title(loc);
+                            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+
+                            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 5);
+                            gMap.animateCamera(cameraUpdate);
+                            marker = gMap.addMarker(markerOptions);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
 
         return view;
     }
 
+    private void getLocation() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+
+        Task<Location> task = fusedClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
+                    SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+                    assert supportMapFragment != null;
+                    supportMapFragment.getMapAsync(MapsFragment.this);
+                }
+            }
+        });
+    }
+
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        mMap = googleMap;
-        mMap.setOnMapClickListener(this);
-        mMap.setOnMapLongClickListener(this);
-
-        LatLng colombia = new LatLng(4.570868, -74.297333);
-        mMap.addMarker(new MarkerOptions().position(colombia).title("Bogotá"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(colombia));
+        gMap = googleMap;
+        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("My Current Location");
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5));
+        googleMap.addMarker(markerOptions);
     }
 
     @Override
-    public void onMapClick(@NonNull LatLng latLng) {
-        txtLatitud.setText(String.valueOf(latLng.latitude));
-        txtLongitud.setText(String.valueOf(latLng.longitude));
-        mMap.clear();
-        LatLng clickedLocation = new LatLng(latLng.latitude, latLng.longitude);
-        mMap.addMarker(new MarkerOptions().position(clickedLocation).title(""));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(clickedLocation));
-    }
-
-    @Override
-    public void onMapLongClick(@NonNull LatLng latLng) {
-        txtLatitud.setText(String.valueOf(latLng.latitude));
-        txtLongitud.setText(String.valueOf(latLng.longitude));
-        mMap.clear();
-        LatLng clickedLocation = new LatLng(latLng.latitude, latLng.longitude);
-        mMap.addMarker(new MarkerOptions().position(clickedLocation).title(""));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(clickedLocation));
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLocation();
+            }
+        }
     }
 }
